81de78f632c65ac7ddb1147b8dcf1c9ac01e5c1b
Orhan Kahraman <herorhan999@gmail.com>
2024-08-29 02:22:02 +0300

    Updates

---
+++ assets/index-B__RKNsE.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CZIDgfH7.js";const s=()=>e.jsx("div",{children:e.jsx("h1",{children:"Page 2"})});export{s as default};
---
+++ assets/index-DRCnsB0R.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CZIDgfH7.js";const s=()=>e.jsx("div",{children:e.jsx("h1",{children:"Page 1"})});export{s as default};
