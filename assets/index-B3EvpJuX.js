116651b3ccdb4733e81036a6e66b112bec7d4139
Orhan Kahraman <herorhan999@gmail.com>
2024-08-30 16:50:56 +0300

    Updates

---
+++ assets/index-B3EvpJuX.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-B4kX2FOT.js";const s=()=>e.jsx("div",{className:"flex justify-center items-center h-full",children:e.jsx("h1",{className:"text-8xl font-bold",children:"Page 2"})});export{s as default};
--- assets/index-DNltB4tu.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BeX0nQF2.js";const s=()=>e.jsx("div",{children:e.jsx("h1",{children:"Page 1"})});export{s as default};
